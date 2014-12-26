b8ccd8545f5e56c1e29f5304379f24c88a780f56
Robert David <robert.david.public@gmail.com>
2014-12-26 13:30:46 +0100

    some cleanups and setup

--- mpguino.c
+++ mpguino.c
@@ -227,7 +227,7 @@ Buttons - Parallax 5-position switch (diagram based on josemapiro efforts)
 //#define TinkerkitLCDmodule true
 
 // if the below #define is commented out, 16 MHz system clock will be assumed
-#define use20MHz true // force 20 MHz system clock values
+#define use20MHz true				// force 20 MHz system clock values
 
 // only one of the below LCD options may be chosen - choosing more than one will cause a compilation error to occur
 // if TinkerkitLCDmodule is used, useLegacyLCD will automatically be used, and the below options will be ignored
@@ -241,7 +241,7 @@ Buttons - Parallax 5-position switch (diagram based on josemapiro efforts)
 
 // the below options only work if useLegacyLCD is selected. If useLegacyLCD is not selected, the below options will not be inserted at all
 //#define useLegacyLCDinvertedBrightness true	// For alternate LCD backlight connections
-#define useLegacyLCDbuffered true				// Speed up LCD output
+#define useLegacyLCDbuffered true		// Speed up LCD output
 
 // selectable options - all may be chosen independently of one another, save for serial data logging.
 // the serial data logging option will conflict with the Parallax LCD output option, if both are selected at the same time
@@ -253,16 +253,16 @@ Buttons - Parallax 5-position switch (diagram based on josemapiro efforts)
 #define useWindowFilter true			// Smooths out "jumpy" instant FE figures that are caused by modern OBDII engine computers
 #define useBigFE true				// Show big fuel economy displays
 #define useBigDTE true				// Show big distance-to-empty displays
-#define useBigTTE true				// Show big time-to-empty displays
-#define useClock true				// Show system clock, and provide means to set it
+//#define useBigTTE true			// Show big time-to-empty displays
+//#define useClock true				// Show system clock, and provide means to set it
 #define useSavedTrips true			// Ability to save current or tank trips to any one of 10 different trip slots in EEPROM
 #define useScreenEditor true			// Ability to change any of 8 existing trip data screens, with 4 configurable figures on each screen
 #define useBarFuelEconVsTime true		// Show Fuel Economy over Time bar graph
 #define useBarFuelEconVsSpeed true		// Show Fuel Economy vs Speed, Fuel Used vs Speed bar graphs
 #define useSpiffyBigChars true
-#define useFuelCost true			// Show fuel cost
-#define useChryslerMAPCorrection true		// Ability to perform on-the-fly fuel injector data correction for late-model Chrysler vehicles
-//#define useABresultViewer true			// Ability to graphically show current (B) versus stored (A) fuel consumption rates
+//#define useFuelCost true			// Show fuel cost
+//#define useChryslerMAPCorrection true		// Ability to perform on-the-fly fuel injector data correction for late-model Chrysler vehicles
+//#define useABresultViewer true		// Ability to graphically show current (B) versus stored (A) fuel consumption rates
 //#define useCoastDownCalculator true		// Ability to calculate C(rr) and C(d) from coastdown
 
 // program measurement and debugging tools
@@ -270,12 +270,12 @@ Buttons - Parallax 5-position switch (diagram based on josemapiro efforts)
 //#define useDebugReadings true
 //#define forceEEPROMsettingsInit true
 //#define useEEPROMviewer true			// Ability to directly examine EEPROM
-//#define useBenchMark true				// this is probably broken - last time I used it was in August 2013
+//#define useBenchMark true			// this is probably broken - last time I used it was in August 2013
 //#define useSerialDebugOutput true
 
 // SWEET64 configuration/debugging
 //#define useSWEET64trace true			// Ability to view real-time 64-bit calculations from SWEET64 kernel
-//#define useSWEET64multDiv true			// shift mul64 and div64 from native C++ to SWEET64 bytecode
+//#define useSWEET64multDiv true		// shift mul64 and div64 from native C++ to SWEET64 bytecode
 
 // these #defines are used to select various features to support the above choices
 // do not mess with them, or compilation errors will occur
